c9697678415b6c8182090e7b0dafa36900227d4b
Joey Frohikey <frohikey@gmail.com>
2018-10-24 18:09:07 +0200

    Fix jpg support for 7.0 and 7.2

---
+++ ViperPhp7.2/push.cmd
@@ -0,0 +1,4 @@
+docker tag viper-php-7.2 goto10hq/viper-php-7.2:1.1.0
+docker tag viper-php-7.2 goto10hq/viper-php-7.2:latest
+docker push goto10hq/viper-php-7.2:1.1.0
+docker push goto10hq/viper-php-7.2:latest
---
+++ ViperPhpCli7.2/push.cmd
@@ -0,0 +1,4 @@
+docker tag viper-php-cli-7.2 goto10hq/viper-php-cli-7.2:1.0.0
+docker tag viper-php-cli-7.2 goto10hq/viper-php-cli-7.2:latest
+docker push goto10hq/viper-php-cli-7.2:1.0.0
+docker push goto10hq/viper-php-cli-7.2:latest
