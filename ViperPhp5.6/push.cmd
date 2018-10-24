c859953911db6981243067b1823a05dc8de509d2
Joey Frohikey <frohikey@gmail.com>
2018-10-24 17:45:48 +0200

    Viper Php 5.6 1.1.0

--- ViperPhp5.6/push.cmd
+++ ViperPhp5.6/push.cmd
@@ -1,4 +1,4 @@
-docker tag viper-php-5.6 goto10hq/viper-php-5.6:1.0.0
+docker tag viper-php-5.6 goto10hq/viper-php-5.6:1.1.0
 docker tag viper-php-5.6 goto10hq/viper-php-5.6:latest
-docker push goto10hq/viper-php-5.6:1.0.0
+docker push goto10hq/viper-php-5.6:1.1.0
 docker push goto10hq/viper-php-5.6:latest
