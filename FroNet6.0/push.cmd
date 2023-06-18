a060291b277dd1af28af2c8ec291f3c11496fdc8
frohikey <frohikey@gmail.com>
2023-06-18 11:20:58 +0200

    new node.js

--- FroNet6.0/push.cmd
+++ FroNet6.0/push.cmd
@@ -1,8 +1,8 @@
-docker tag fro-aspnet-6.0 goto10hq/fro-aspnet-6.0:1.2.0
+docker tag fro-aspnet-6.0 goto10hq/fro-aspnet-6.0:1.3.0
 docker tag fro-aspnet-6.0 goto10hq/fro-aspnet-6.0:latest
-docker push goto10hq/fro-aspnet-6.0:1.2.0
+docker push goto10hq/fro-aspnet-6.0:1.3.0
 docker push goto10hq/fro-aspnet-6.0:latest
-docker tag fro-sdk-6.0 goto10hq/fro-sdk-6.0:1.2.0
+docker tag fro-sdk-6.0 goto10hq/fro-sdk-6.0:1.3.0
 docker tag fro-sdk-6.0 goto10hq/fro-sdk-6.0:latest
-docker push goto10hq/fro-sdk-6.0:1.2.0
+docker push goto10hq/fro-sdk-6.0:1.3.0
 docker push goto10hq/fro-sdk-6.0:latest
