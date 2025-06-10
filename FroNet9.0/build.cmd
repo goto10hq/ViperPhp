1e2075233f8b694a594ab4139f05cb95854d2fa8
frohikey <frohikey@gmail.com>
2025-06-10 10:11:00 +0200

    Fro9.0

---
+++ FroNet9.0/build.cmd
@@ -0,0 +1,2 @@
+docker build -t fro-aspnet-9.0 -f Dockerfile1 .
+docker build -t fro-sdk-9.0 -f Dockerfile2 .
---
+++ FroNet9.0/push.cmd
@@ -0,0 +1,8 @@
+docker tag fro-aspnet-9.0 goto10hq/fro-aspnet-9.0:1.0.0
+docker tag fro-aspnet-9.0 goto10hq/fro-aspnet-9.0:latest
+docker push goto10hq/fro-aspnet-9.0:1.0.0
+docker push goto10hq/fro-aspnet-9.0:latest
+docker tag fro-sdk-9.0 goto10hq/fro-sdk-9.0:1.0.0
+docker tag fro-sdk-9.0 goto10hq/fro-sdk-9.0:latest
+docker push goto10hq/fro-sdk-9.0:1.0.0
+docker push goto10hq/fro-sdk-9.0:latest
