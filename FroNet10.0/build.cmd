f67f4d3ee7a190c359b48a351f44fc6f0eb16d12
frohikey <frohikey@gmail.com>
2026-09-10 10:48:10 +0200

    viper-php-8.4:1.1.0

---
+++ FroNet10.0/build.cmd
@@ -0,0 +1,2 @@
+docker build -t fro-aspnet-10.0 -f Dockerfile1 .
+docker build -t fro-sdk-10.0 -f Dockerfile2 .
---
+++ FroNet10.0/push.cmd
@@ -0,0 +1,8 @@
+docker tag fro-aspnet-10.0 goto10hq/fro-aspnet-10.0:1.0.0
+docker tag fro-aspnet-10.0 goto10hq/fro-aspnet-10.0:latest
+docker push goto10hq/fro-aspnet-10.0:1.0.0
+docker push goto10hq/fro-aspnet-10.0:latest
+docker tag fro-sdk-10.0 goto10hq/fro-sdk-10.0:1.0.0
+docker tag fro-sdk-10.0 goto10hq/fro-sdk-10.0:latest
+docker push goto10hq/fro-sdk-10.0:1.0.0
+docker push goto10hq/fro-sdk-10.0:latest
